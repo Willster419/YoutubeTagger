24932fa08a41835ac7109dc74f285175a66383bc
Willster419 <willster419@outlook.com>
2019-01-07 17:42:43 -0500

    ran the script today

--- YoutubePlaylistAlbumDownload/bin/Debug/EDM_Treasures/part_playlist_download_settings.bat
+++ YoutubePlaylistAlbumDownload/bin/Debug/EDM_Treasures/part_playlist_download_settings.bat
@@ -1,1 +1,1 @@
-youtube-dl -i --playlist-reverse --dateafter 20181226 -o "%%(autonumber)s-%%(title)s.%%(ext)s" --format m4a --embed-thumbnail https://www.youtube.com/playlist?list=UUIGtPvEVzyHdn01m4P36_yQ
+youtube-dl -i --playlist-reverse --dateafter 20190107 -o "%%(autonumber)s-%%(title)s.%%(ext)s" --format m4a --embed-thumbnail https://www.youtube.com/playlist?list=UUIGtPvEVzyHdn01m4P36_yQ
--- YoutubePlaylistAlbumDownload/bin/Debug/Eric_Clapman/part_playlist_download_settings.bat
+++ YoutubePlaylistAlbumDownload/bin/Debug/Eric_Clapman/part_playlist_download_settings.bat
@@ -1,1 +1,1 @@
-youtube-dl -i --playlist-reverse --dateafter 20181226 -o "%%(autonumber)s-%%(title)s.%%(ext)s" --format m4a --embed-thumbnail https://www.youtube.com/user/ericclapman/videos
+youtube-dl -i --playlist-reverse --dateafter 20190107 -o "%%(autonumber)s-%%(title)s.%%(ext)s" --format m4a --embed-thumbnail https://www.youtube.com/user/ericclapman/videos
