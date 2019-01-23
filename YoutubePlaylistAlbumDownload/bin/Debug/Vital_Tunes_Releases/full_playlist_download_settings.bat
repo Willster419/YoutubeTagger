135855b8682ad9500c92643bf78cbe5212269e43
Willster419 <willster419@outlook.com>
2019-01-22 23:09:23 -0700

    whoops

--- YoutubePlaylistAlbumDownload/bin/Debug/Vital_Tunes_Releases/full_playlist_download_settings.bat
+++ YoutubePlaylistAlbumDownload/bin/Debug/Vital_Tunes_Releases/full_playlist_download_settings.bat
@@ -1,1 +1,1 @@
-youtube-dl -i --playlist-reverse -o "%%(autonumber)s-%%(title)s.%%(ext)s" --format m4a --embed-thumbnail https://www.youtube.com/user/VitalTunezMusic/videos
+youtube-dl -i --playlist-reverse --match-filter "duration < 600" -o "%%(autonumber)s-%%(title)s.%%(ext)s" --format m4a --embed-thumbnail https://www.youtube.com/user/VitalTunezMusic/videos
